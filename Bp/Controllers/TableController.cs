fcceb137eeb4dfa9ba8207eb78d9ec95d1753bb2
王子瑶 <727841493@qq.com>
2018-06-05 00:58:48 +0800

    0

--- Bp/Controllers/TableController.cs
+++ Bp/Controllers/TableController.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace Bp.Controllers
@@ -575,26 +576,10 @@ namespace Bp.Controllers
 
                     string info = target + "\\" + "Upload_File_State.inf";
                     string vTime = time.Year + "-" + time.Month + "-" + time.Day + " " + time.Hour + ":" + time.Minute + ":" + time.Second;
-                    StringBuilder str = new StringBuilder();
-                    str.Append("[基本信息]" + "\r\n");
-                    str.Append("上传文件名=" + filename + "\r\n");
-                    str.Append("上传类型=项目" + "\r\n");
-                    str.Append("上传文件大小=" + files.ContentLength+"\r\n");
-                    str.Append("上传日期=" + vTime + "\r\n");
-                    str.Append("上传登录名=" + user.登录名 + "\r\n");
-                    str.Append("上传用户=" + user.用户姓名 + "\r\n");
-                    str.Append("上传计算机=" + clientPCName + "\r\n");
-                    str.Append("是否逻辑删除=0");
-                    StreamWriter sw;
-                    if (!System.IO.File.Exists(info))
-                    {
-                        sw = System.IO.File.CreateText(info);
-                    }
-                    else
-                    {
-                        sw = System.IO.File.AppendText(info);
-                    }
-                    sw.WriteLine(str.ToString());
+
+                    FileStream fs = new FileStream(info, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.WriteLine("[基本信息]", new Object[] { "上传文件名=" + filename, "上传类型=项目", "上传文件大小=" + files.ContentLength, "上传日期=" + vTime, "上传登录名=" + user.登录名, "上传用户=" + user.用户姓名, "上传计算机=" + clientPCName, "是否逻辑删除=0" });
                     sw.Close();
 
                     Bp_项目资料 xmzl = new Bp_项目资料
